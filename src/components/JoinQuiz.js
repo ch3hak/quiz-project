a374386f7f0507f3c5414116048c9bd2698044d1
Chehak Sharma <ch3hak@gmail.com>
2025-02-09 18:07:04 +0530

    join quiz functionality

--- src/components/JoinQuiz.js
+++ src/components/JoinQuiz.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 
 const JoinQuiz = () => {
   const navigate = useNavigate();
+  const [quizCode, setQuizCode] = useState("");
   return (
     <div>JoinQuiz
       <p onClick={ () => navigate("/create-quiz")}>Create a quiz instead.</p>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Enter Quiz Code"
+          value={quizCode}
+          onChange={(e) => setQuizCode(e.target.value)}
+        />
+        <button onClick={ () => navigate(`/quiz/${quizCode}`)}>Join</button>
+      </form>
     </div>
   )
 }
 
-export default JoinQuiz
+export default JoinQuiz;
